300781f2f421a6b398a6cbf2a4ab6187068ed978
7Am1 <7Am1@proton.me>
2024-07-31 19:39:44 +0200

    Update blocks.h

--- .config/dwmblocks/blocks.h
+++ .config/dwmblocks/blocks.h
@@ -1,12 +1,13 @@
+#define SC(directorio) "$HOME/.config/dwmblocks/scripts/" #directorio
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	//{"",	"/home/carti/.config/dwmblocks/scripts/cpu.sh",	        	3,	            0  },
-	{"",	"/home/carti/.config/dwmblocks/scripts/volume.sh",	        0,	            10 },
-	{"",	"/home/carti/.config/dwmblocks/scripts/battery.sh",	        15,	            0  },
-	{"",	"/home/carti/.config/dwmblocks/scripts/netconn.sh",	        15,	            0  },
-	{"",	"/home/carti/.config/dwmblocks/scripts/date.sh",	        15,	            0  },
-	{"",	"/home/carti/.config/dwmblocks/scripts/time.sh",	        15,	            0  },
+	{"",	SC("volume.sh"),	        0,	            10 },
+	{"",	SC("battery.sh"),	        15,	            0  },
+	{"",	SC("netconn.sh"),	        15,	            0  },
+	{"",	SC("date.sh"),	        15,	            0  },
+	{"",	SC("time.sh"),	        15,	            0  },
 };
 
 //sets delimiter between st1atus commands. NULL character ('\0') means no delimiter.
